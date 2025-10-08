7f60f4536a09174de45070b65ab13929815ed488
Anna Wahl <123816763+awahl2@users.noreply.github.com>
2025-10-08 13:11:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Use "flask --app app run" To Run
 
-from flask import Flask
+from flask import Flask, jsonify
 import os
 from dotenv import load_dotenv, dotenv_values 
 import requests
@@ -8,36 +8,80 @@ import requests
 app = Flask(__name__)
 
 
-# Load Variables From .env File
+# Load env file
 load_dotenv()
 
-# Global Variables
+# Aplos global variables
 aplos_token_url = os.getenv("APLOS_TOKEN_URL")
 aplos_id = os.getenv("APLOS_ID")
 aplos_secret = os.getenv("APLOS_SECRET")
 
-# Get An Access Token
-def get_access_token():
+# Virtuous global variables
+virtuous_token_url = os.getenv("VIRTUOUS_TOKEN_URL")
+virtuous_username = os.getenv("VIRTUOUS_USERNAME")
+virtuous_password = os.getenv("VIRTUOUS_PASSWORD")
 
-    # Exchanges ID, Secret for Access Token
+
+# Gets access token
+def get_access_token_aplos():
+
+    # Gets data from .env file
     data = {
         "grant_type": "client_credentials",
         "client_id": aplos_id,
         "client_secret": aplos_secret
     }
 
-    response = requests.post(aplos_token_url)
+    response = requests.post(aplos_token_url, data=data, headers={
+        "Content-Type": "application/x-www-form-urlencoded"
+    })
 
-    # Checks for Codes Other Than 200 (200 Code: API Accessed Successfully)
+    # Checks for codes other than 200 (200 Code: API Accessed Successfully)
     if response.status_code != 200:
         raise Exception(f"Failed to get access token: {response.status_code} {response.text}")
 
     token_info = response.json()
+
+    # Return token from json
+    return token_info["data"]["token"]
+
+def get_access_token_virtuous():
+
+    # Gets data from .env file
+    data = {
+        "grant_type": "password",
+        "username": virtuous_username,
+        "password": virtuous_password
+    }
+
+    response = requests.post(virtuous_token_url, data=data, headers={
+        "Content-Type": "application/x-www-form-urlencoded"
+    })
+
+    # Checks for codes other than 200 (200 Code: API Accessed Successfully)
+    if response.status_code != 200:
+        raise Exception(f"Failed to get Virtuous token: {response.status_code} {response.text}")
+
+    token_info = response.json()
+
+    # Return token from json
     return token_info["access_token"]
 
 @app.route("/")
-def hello_world():
-    print("Requesting access token...")
-    token = get_access_token()
-    print("Access token received!\n")
-    return token
+def main():
+    #Initialize token list
+    token_list = []
+
+    # Request access token from aplos
+    print("Requesting access token from Aplos...")
+    applos_token = get_access_token_aplos()
+    print("Access token from Aplos received!\n")
+    token_list.append(applos_token)
+
+    # Request access token from virtuous
+    print("Requesting access token from Virtuous...")
+    virtuous_token = get_access_token_virtuous()
+    print("Access token from Virtuous received!\n")
+    token_list.append(virtuous_token)
+
+    return jsonify(token_list)
